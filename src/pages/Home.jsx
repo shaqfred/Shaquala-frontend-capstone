08ec17a185054b99b4feeef3026754708ff48be7
shaquala <96317236+shaqfred@users.noreply.github.com>
2024-04-10 09:58:18 -0400

    added more features

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,11 +4,14 @@ import "./Home.css"
 
 function Home(){
     return(
-        <div className="home">
+
+
+        <div className="home" >
+            <p className="text-transparent">
             <h1> Welcome to Journalfly</h1>
-            <h1>"Self-Awareness, Self-Checkin, Self-Care,</h1>
-            <h2>and all in your pocket"</h2>
-            <h1>How are you feeling today?</h1>
+            <h1>"Self-Awareness, Self-Checkin, Self-Care</h1>
+            {/* <h2>and all in your pocket"</h2> */}
+            </p>
         </div>
     )
 }
