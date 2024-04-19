8509df31d1b54c463db1f46a08b03f0a01c41075
shaquala <96317236+shaqfred@users.noreply.github.com>
2024-04-19 05:29:11 -0400

    added the png to home link

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
+// import{Link}from "react-router-dom"
 import "./Home.css"
 
 
-function Home(){
+function HomePage(){
     return(
 
 
         <div className="home" >
+{/* <Link href="home-page_url"><img src="https://trello.com/1/cards/65f92e9190ad46baa763a9a9/attachments/65f92f3085bd4df25f395459/previews/65f92f3085bd4df25f3954eb/download/image.png" alt="Home"/></Link> */}
 
             <h1> Welcome to Journalfly</h1>
             <h1>"Self-Awareness, Self-Check in, Self-Care,</h1>
@@ -16,5 +18,5 @@ function Home(){
          
         </div>
     )
-}
-export default Home
+    }
+export default HomePage
