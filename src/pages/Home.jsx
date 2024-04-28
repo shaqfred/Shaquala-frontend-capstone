476977ad72312230ed5081cd4d3c5b22e29f7cca
shaquala <96317236+shaqfred@users.noreply.github.com>
2024-04-28 12:06:22 -0400

    home page css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,16 +6,16 @@ function HomePage() {
   return (
     <div className="home">
       {/* <Link href="home-page_url"><img src="https://trello.com/1/cards/65f92e9190ad46baa763a9a9/attachments/65f92f3085bd4df25f395459/previews/65f92f3085bd4df25f3954eb/download/image.png" alt="Home"/></Link>  */}
+ <h1>Welcome To Journalfly</h1>
+      <span>
+     
+        How are you feeling?<br/> What mood are you in? <br/>Are you looking for
+        encouragement?<br/>
+      </span>
 
-      <h1>
-        How are you feeling? What mood are you in? Are you looking for
-        encouragement?
-      </h1>
-
-      <h2>Welcome To Journalfy</h2>
-      <h2>"Self-Awareness, Self-Check in, Self-Care,</h2>
-      <h2>and all on your screen!"</h2>
-    </div>
+      <span>"Self-Awareness, Self-Check in, Self-Care,</span>
+      <span>and all on your screen!"</span></div>
+    
   );
 }
 export default HomePage;
